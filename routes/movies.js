df7d24eabfa97eca0e6e4bcdc9749ce2149dc22d
mouad01laayouni <mouad01laayouni@gmail.com>
2022-01-21 12:19:37 +0100

    ajout du prisma

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,22 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+ 
+router.get('/:take:skip', async function(req, res, next) {
+    const movies = await prisma.movies.findMany({
+        skip:10,
+        take: 10
+        })
+  res.send({
+      data: movies,
+      pagination : {
+          count : total, // Total des enregistrements
+          take: limit,   // Nombre d'éléments sélectionnés
+          skip: offset   // Décalage à partir duquel on prend les  données
+      }
+  });
 
-/* GET movies listing. */
-router.get("/", function (req, res, next) {
-  var movies = require("../movies_reduit.json");
-  res.send(movies);
 });
-
+ 
 module.exports = router;
